21a7eee7947d4f006be5279b00f10b7aa9a923e8
DeVerl Stokes <deverl_stokes@intuit.com>
2024-04-20 11:03:20 -0600

    Modified output.

--- frequentNumbers.php
+++ frequentNumbers.php
@@ -20,12 +20,44 @@ function getFrequentNumbers(array $nums, int $k) : array {
     // Take the first k keys from the list.
     return array_slice(array_keys($m), 0, $k);
 }
-    
-    
-    
-$nums = [9,3,9,3,9,3,7,9,7,2,9,4,4,9,4,4,4,9,9,8,8,6,6,1,1,1,1,1,1];
-$k = 4;
 
-$l = getFrequentNumbers($nums, $k);
 
-print_r($l);
+function printArray(string $prefix, array $a) {
+    echo "$prefix [ ";
+    foreach ($a as $n) {
+        echo "$n ";
+    }
+    echo "]" . PHP_EOL;
+}
+    
+
+function runTest(array $nums, int $k) {
+    $l = getFrequentNumbers($nums, $k);
+
+    echo PHP_EOL;
+    printArray("  nums:", $nums);
+    echo "     k: $k" . PHP_EOL;
+    printArray("result:", $l);
+}
+
+function runAllTests() {
+    $nums1 = [1,1,1,2,2,3];
+    $k1 = 2;
+
+    runTest($nums1, $k1);
+
+    $nums2 = [1,4,2,5,7,5,4,4,5,5,5,2,7,2,5,4];
+    $k2 = 3;
+
+    runTest($nums2, $k2);
+
+    $nums3 = [9,3,9,3,9,3,7,9,7,2,9,4,4,9,4,4,4,9,9,8,8,6,6,1,1,1,1,1,1];
+    $k3 = 4;
+
+    runTest($nums3, $k3);
+
+    echo PHP_EOL;
+}
+    
+
+runAllTests();
